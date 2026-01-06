863be51a517b1e1a83a345b49765b1530aae742f
shaikhhuda919-debug <shaikhhuda919@gmail.com>
2026-01-06 13:25:54 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$conn = mysqli_connect("localhost", "root", "", "Quizmaster_codsoft");
-
-if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
-}
-?>
